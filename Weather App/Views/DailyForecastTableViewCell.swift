907d17c402b6cd5801c8fd90d8c15972afc0eb16
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-06-27 20:36:43 -0400

    class implemented in TVC

---
+++ Weather App/Views/DailyForecastTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DailyForecastTableViewCell.swift
+//  Weather App
+//
+//  Created by argenis delarosa on 6/27/19.
+//  Copyright © 2019 argenis delarosa. All rights reserved.
+//
+
+import UIKit
+
+class DailyForecastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
